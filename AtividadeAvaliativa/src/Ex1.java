39e9e08b07689b783c92c2e061f6b5825b517212
Matheus Valentim <0001074796@senaimgaluno.com.br>
2024-11-05 16:56:32 -0300

    Adicionando arquivos

--- AtividadeAvaliativa/src/Ex1.java
+++ AtividadeAvaliativa/src/Ex1.java
@@ -4,11 +4,44 @@ a. a maior e a menor altura do grupo;
 b. média de altura dos homens;
 c. o número de mulheres.
  */
+import java.util.Scanner;
 public class Ex1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner input = new Scanner (System.in);
+		double[] altura = new double[10];
+        String[] sexo = new String[10];
 
-	}
+        double maior_altura = 0;
+        double menor_altura = 0;
+        int quantidadeFeminino = 0;
 
+        for (int i = 0; i < 10; i++) {
+            System.out.print("Digite sua altura: ");
+            altura[i] = input.nextDouble();
+
+            System.out.print("Digite seu sexo (M/F): ");
+            sexo[i] = input.next();
+
+            if (altura[i] > maior_altura) {
+            	maior_altura = altura[i];
+            }
+            if (altura[i] < menor_altura) {
+            	menor_altura = altura[i];
+            }
+
+            if (sexo[i].equalsIgnoreCase("F")) {
+                quantidadeFeminino++;
+            }
+        }
+        
+        double media_altura = altura[0] / 10;
+
+        System.out.println("Maior altura: " + maior_altura);
+        System.out.println("Menor altura: " + menor_altura);
+        System.out.println("Media altura: " + media_altura);
+        System.out.println("Quantidade de pessoas do sexo feminino: " + quantidadeFeminino);
+        
+        input.close();
+    }
 }
--- AtividadeAvaliativa/src/Ex2.java
+++ AtividadeAvaliativa/src/Ex2.java
@@ -1,11 +1,27 @@
 /*Criar um programa que calcule a média de salários de uma empresa, 
  * pedindo ao usuário a grade de funcionários e os salários, e devolvendo a média salarial.
  */
+import java.util.Scanner;
 public class Ex2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner input = new Scanner(System.in);
+		
+		
+		
+		System.out.println("Informe a grade de funcionários: ");
+		int grade_func = input.nextInt();
+		double salarios = 0;
+		
+		for(int i = 1; i <= grade_func; i++) {
+		System.out.println("Informe os salários: ");
+		salarios += input.nextDouble();
+		
+			}
+		double media_salarial = salarios / grade_func ;
+		System.out.println("Media salárial: " + media_salarial);
+		
+		input.close();
 	}
 
 }
