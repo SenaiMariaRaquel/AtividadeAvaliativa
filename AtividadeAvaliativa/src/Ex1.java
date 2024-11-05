ae9bf19505b8957581fcbdd996312c69d1e367f7
Gabriel Fakelmann <0001068776@senaimgaluno.com.br>
2024-11-05 15:30:34 -0300

    Atividade Avaliativa Gabriel Fakelmann

--- AtividadeAvaliativa/src/Ex1.java
+++ AtividadeAvaliativa/src/Ex1.java
@@ -4,10 +4,46 @@ a. a maior e a menor altura do grupo;
 b. média de altura dos homens;
 c. o número de mulheres.
  */
+//Desenvolvido por: Gabriel Fakelmann
+import java.util.Scanner;
+
 public class Ex1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		String sexo;
+		float maior = -9999, menor = 9999, altura;
+		int num_mulheres = 0;
+		double media_masc = 0, cont = 0, media_final;
+
+		for (int i = 1; i <= 10; i++) {
+			System.out.println("Digite a altura: ");
+			altura = sc.nextFloat();
+			if (altura > maior) {
+				maior = altura;
+			} else if (altura < menor) {
+				menor = altura;
+			}
+
+			sc.nextLine();
+			System.out.println("Digite o sexo (Masculino ou Feminino): ");
+			sexo = sc.nextLine();
+
+			if (sexo.equalsIgnoreCase("Feminino")) {
+				num_mulheres++;
+			} else if (sexo.equalsIgnoreCase("Masculino")) {
+				media_masc = media_masc + altura;
+				cont++;
+			}
+
+		}
+		media_final = media_masc / cont;
+		System.out.println("Maior altura do grupo foi: " + maior);
+		System.out.println("Menor altura do grupo foi: " + menor);
+		System.out.printf("Media da altura dos homens foi de:%.2f%n ", media_final);
+		System.out.printf("Numero de mulheres: " + num_mulheres);
+
+		sc.close();
 
 	}
 
--- AtividadeAvaliativa/src/Ex2.java
+++ AtividadeAvaliativa/src/Ex2.java
@@ -1,11 +1,37 @@
 /*Criar um programa que calcule a média de salários de uma empresa, 
  * pedindo ao usuário a grade de funcionários e os salários, e devolvendo a média salarial.
  */
+//Desenvolvido por: Gabriel Fakelmann
+import java.util.Scanner;
+
 public class Ex2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		int cont = 0;
+		double salario = 0, media = 0, media_final = 0;
+		
+		System.out.print("Há quantos funcionarios na empresa: ");
+		int quant_funci = sc.nextInt();
+		
+		for(int i = 1; i <= quant_funci;i++) {
+			System.out.print("Digite o salario do "+ (i)+ "° funcionário: ");
+			salario = sc.nextDouble();
+			if (salario > 0) {
+				media = media + salario;
+				cont++;
+			}
+			else {
+				System.out.println("Dados Inválido");
+				i--;
+			}
+				
+			}
+		media_final = media / cont;
+		System.out.printf("A média salarial dos funcionários da empresa é R$:%.2f%n" , media_final );
 
+		 sc.close();
 	}
 
-}
+	}
