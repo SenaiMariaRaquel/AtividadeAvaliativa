ea688d26024c90fad2a811df290e3dbacc10f551
Diogo <0001085533@senaimgaluno.com.br>
2024-11-05 15:45:55 -0300

    Adicionando exercicios

--- AtividadeAvaliativa/src/Ex1.java
+++ AtividadeAvaliativa/src/Ex1.java
@@ -4,11 +4,46 @@ a. a maior e a menor altura do grupo;
 b. média de altura dos homens;
 c. o número de mulheres.
  */
+
+import java.util.Scanner;
+
 public class Ex1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		
+		int cont_F = 0;
+		double cont_altura = 0;
+		double maior = 0;
+		double menor = 9999;
+		
 
+		for (int i = 0; i <= 10; i++) 
+		{
+			System.out.println("Digite F para FEMININO ou M para MASCULINO: ");
+			String sexo = scanner.nextLine();
+
+			
+			System.out.println("Digite sua altura: ");
+			double altura = scanner.nextDouble();
+			
+			if(sexo.equals("F"))
+			{
+				cont_F++;
+			}
+			
+			if(sexo.equals("M"))
+			{
+				
+				
+			}
+			
+			
+			
+			
+		}
+           System.out.println("Número de mulheres: " + cont_F + "\n" + "média de altura dos homens: " + "\n" + "Maior de altura: " + "Menor de altura: ");
+			
 	}
 
 }
