a1cf0bbf4c6055ca72db553cb0fc14b94f5cd4ae
GustavodeCastrov <0001074799@senaimgaluno.com.br>
2024-11-05 16:36:20 -0300

    Enviando EX1 e EX2

--- AtividadeAvaliativa/src/Ex2.java
+++ AtividadeAvaliativa/src/Ex2.java
@@ -1,11 +1,23 @@
 /*Criar um programa que calcule a média de salários de uma empresa, 
  * pedindo ao usuário a grade de funcionários e os salários, e devolvendo a média salarial.
  */
+import java.util.Scanner;
 public class Ex2 {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sca = new Scanner(System.in);
+		
+		double sala, media = 0;
+		
+		System.out.print("Digite a quantidade de funcionários: ");
+		int fun = sca.nextInt();
+		
+		for(int i = 1; i <= fun; i++) {
+			System.out.println("Digite o salário do "+i+"º funcionário: ");
+			sala = sca.nextDouble();
+			
+			media = (sala+sala)/fun;
+		}
+		System.out.println("A media de salário é: "+media);
 	}
 
 }
