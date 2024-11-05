cb8490ed1e6977ec4e45070e8ec8c2b572c52f87
CaioAlves <0000983806@senaimgaluno.com.br>
2024-11-05 17:06:08 -0300

    exe 25

--- AtividadeAvaliativa/src/Ex2.java
+++ AtividadeAvaliativa/src/Ex2.java
@@ -1,11 +1,42 @@
 /*Criar um programa que calcule a média de salários de uma empresa, 
  * pedindo ao usuário a grade de funcionários e os salários, e devolvendo a média salarial.
  */
+
+import java.util.Scanner;
 public class Ex2 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
+			public static void main(String[] args) {
+				Scanner sc = new Scanner(System.in);
 
-	}
+				double salarios, media = 0, media_final = 0;
+				int cont = 0;
 
-}
+				System.out.print("Quantos funcionários há na empresa: ");
+				int quant_fun = sc.nextInt();
+
+				System.out.println();
+				for (int i = 1; i <= quant_fun; i++) {
+					System.out.print("Digite o salário do funcionário " + i + ": ");
+					salarios = sc.nextDouble();
+					if (salarios > 0) {
+						media = media + salarios;
+						cont++;
+					} else {
+						System.out.println("Dados inválidos.");
+						i--;
+					}
+				}
+				media_final = media / cont;
+				System.out.printf("A média salarial da empresa é de: R$%.2f", media_final);
+
+
+			}
+
+		}
+
+
+
+	
+
+
